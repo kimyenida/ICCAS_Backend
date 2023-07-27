72084e8a779797d6cfee3d53351368c63dcdd3c2
Yunhui <kss7035@naver.com>
2023-07-27 16:33:45 +0900

    DB 연결변경

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -1,22 +1,22 @@
-const maria = require('mysql');
+const maria = require("mysql");
 
 const conn = maria.createConnection({
-    host: '192.168.0.95',
-    port : 3306,
-    user : 'yeni2015',
-    password: 'yeni2015',
-    database: 'i++'
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "qwer1234",
+  database: "i++",
 });
 
 conn.connect;
 
 const queryreturn = (query) => {
-    return new Promise(function(resolve,reject){
-        conn.query(String(query),function(error,results,fields){
-            if(error) throw error;
-            resolve(results); 
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    conn.query(String(query), function (error, results, fields) {
+      if (error) throw error;
+      resolve(results);
+    });
+  });
+};
 
-module.exports = {queryreturn};
+module.exports = { queryreturn };
